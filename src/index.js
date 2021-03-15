998152506d347adff44027e64f5ea52f192ec765
julia2017-yg <julka.getman@gmail.com>
2021-03-15 21:24:30 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  var min = 0;
+  if (Array.isArray(array) && array.length) {
+    min = array[0];
+    array.map(function(value) { 
+      if (value < min) {
+        min = value;
+      }
+    });
+  }
+  return min;        
+},  
 
 exports.max = function max (array) {
-  return 0;
-}
+  var max = 0;
+  if (Array.isArray(array) && array.length) {
+    max = array[0];
+    array.map(function(value) {
+      if (value > max) {
+        max = value;
+      }
+    })
+  }
+  return max;
+
+},
+
 
 exports.avg = function avg (array) {
-  return 0;
+  var avg = 0;
+   if (Array.isArray(array) && array.length) {  
+    array.map(function(value) {    
+      avg += value;
+    });
+    avg = avg / array.length;
+  }
+  return avg;
 }
